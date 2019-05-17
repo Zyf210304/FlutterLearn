138c9da75f09658f15ad352427b25026de63478e
Zyf210304 <672233580@qq.com>
2019-05-17 18:08:47 +0800

    Update camera_page.dart

--- fei_learn_flutter_0/lib/camera_page.dart
+++ fei_learn_flutter_0/lib/camera_page.dart
@@ -13,14 +13,15 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  List<File> _images = [];
 
-  File _image;
-
-  Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+  Future getImage(bool isCamera) async {
+    Navigator.pop(context);
+    var image = await ImagePicker.pickImage(
+        source: isCamera ? ImageSource.camera : ImageSource.gallery);
 
     setState(() {
-      _image = image;
+      _images.add(image);
     });
   }
 
@@ -40,17 +41,81 @@ class _CameraPageState extends State<CameraPage> {
         ),
       ),
       body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
-      ),
+          child: Wrap(
+        spacing: 5,
+        runSpacing: 5,
+        children: _genImages(),
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
+        onPressed: _pickImage,
+        tooltip: '选择图片',
         child: Icon(Icons.add_a_photo),
       ),
     );
   }
-  
-  
+
+  void _pickImage() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              height: 160,
+              child: Column(
+                children: <Widget>[
+                  _item('拍照', true),
+                  _item('相册', false),
+                ],
+              ),
+            ));
+  }
+
+  _item(String title, bool isCamera) {
+    return GestureDetector(
+      child: ListTile(
+        leading: Icon(isCamera ? Icons.camera : Icons.photo_library,
+            color: Colors.blue),
+        title: Text(title),
+        onTap: () => getImage(isCamera),
+      ),
+    );
+  }
+
+  _genImages() {
+    return _images.map((file) {
+      return Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(
+              file,
+              width: 120,
+              height: 90,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Positioned(
+            right: 5,
+            top: 5,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _images.remove(file);
+                });
+              },
+              child: ClipOval(
+                child: Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(color: Colors.black54),
+                  child: Icon(
+                    Icons.close,
+                    size: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    }).toList();
+  }
 }
